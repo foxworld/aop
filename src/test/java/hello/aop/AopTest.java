5cfc024d26ba6edd12bc8f821af6f45363dace55
이훈구 <foxworld@gmail.com>
2025-06-26 09:59:38 +0900

    aop 매개면수전달 test

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -37,7 +37,7 @@ public class AopTest {
         orderService.orderItem("itemA");
     }
 
-    @Test
+    @org.junit.Test
     void exception() {
         Assertions.assertThatThrownBy(() -> orderService.orderItem("ex"))
                 .isInstanceOf(IllegalStateException.class);
